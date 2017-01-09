eeacd1aa1968dbd79ae72363d1c96b2c36890f74
santoshenoy <santoshenoy@gmail.com>
2017-01-09 23:30:59 +0530

    9th Jan

---
+++ src/main/java/com/niit/shoppingCart/controller/ProductController.java
@@ -0,0 +1,5 @@
+package com.niit.shoppingCart.controller;
+
+public class ProductController {
+
+}
---
+++ src/main/java/com/niit/shoppingCart/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.niit.shoppingCart.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.niit.shoppingcart.dao.CategoryDAO;
+import com.niit.shoppingcart.dao.SupplierDAO;
+import com.niit.shoppingcart.dao.UserDAO;
+import com.niit.shoppingcart.model.Category;
+import com.niit.shoppingcart.model.Supplier;
+import com.niit.shoppingcart.model.User;
+
+@Controller
+public class UserController {
+
+	public static Logger log = LoggerFactory.getLogger(UserController.class);
+
+	@Autowired
+	UserDAO userDAO;
+
+	@Autowired
+	User user;
+
+	@Autowired
+	private CategoryDAO categoryDAO;
+
+	@Autowired
+	private Category category;
+
+	@Autowired
+	private SupplierDAO supplierDAO;
+
+	@Autowired
+	private Supplier supplier;
+
+	@Autowired
+	private HttpSession session;
+
+}
